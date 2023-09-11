0a5ff07160130b0035991800df684ac0ba676668
Bryan Pike <107475188+thebobfather2@users.noreply.github.com>
2023-09-10 20:41:31 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ import "./Home.css";
 const Home = () => {
   return (
     <div>
-      <h1 className='welcome'>Welcome!</h1>
-      <h2 className='text-center text-xl font-bold py-8'>Sign in to view community updates!</h2>
+      <h1 className='welcome'>Members Only</h1>
+      <h2 className='text-center text-xl font-bold py-8'>Sign in to access the Deep Notes Ecosystem.</h2>
     </div>
   )
 }
